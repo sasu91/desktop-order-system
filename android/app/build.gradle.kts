87c5bcc5fa15dd3d4e0f753360439a5fc9944693
sasu91 <lautasellanes@hotmail.com>
2026-02-27 13:10:25 +0000

    gredle fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -36,8 +38,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions { jvmTarget = "17" }
 
     buildFeatures {
         compose = true
@@ -95,4 +95,10 @@ dependencies {
 
     // Coroutines
     implementation(libs.coroutines.android)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
